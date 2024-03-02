697eb62255a2264fe8dc67ee2e59bc4eb2f77edb
SajidAhmed0 <mjsahmed205@gmail.com>
2024-03-02 15:44:52 +0530

    roomtype entity

---
+++ src/main/java/com/hotelmangementsystem/application/entity/RoomType.java
@@ -0,0 +1,108 @@
+package com.hotelmangementsystem.application.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.ArrayList;
+
+@Entity
+public class RoomType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Integer noOfRooms;
+
+    private Integer maxAdult;
+
+    private String description;
+
+    private ArrayList<String> facilities;
+
+    private Double price;
+
+    //TODO: create season and contract
+
+    public RoomType() {
+    }
+
+    public RoomType(String name, Integer noOfRooms, Integer maxAdult, String description, ArrayList<String> facilities, Double price) {
+        this.name = name;
+        this.noOfRooms = noOfRooms;
+        this.maxAdult = maxAdult;
+        this.description = description;
+        this.facilities = facilities;
+        this.price = price;
+    }
+
+    public RoomType(Long id, String name, Integer noOfRooms, Integer maxAdult, String description, ArrayList<String> facilities, Double price) {
+        this.id = id;
+        this.name = name;
+        this.noOfRooms = noOfRooms;
+        this.maxAdult = maxAdult;
+        this.description = description;
+        this.facilities = facilities;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNoOfRooms() {
+        return noOfRooms;
+    }
+
+    public void setNoOfRooms(Integer noOfRooms) {
+        this.noOfRooms = noOfRooms;
+    }
+
+    public Integer getMaxAdult() {
+        return maxAdult;
+    }
+
+    public void setMaxAdult(Integer maxAdult) {
+        this.maxAdult = maxAdult;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getFacilities() {
+        return facilities;
+    }
+
+    public void setFacilities(ArrayList<String> facilities) {
+        this.facilities = facilities;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
