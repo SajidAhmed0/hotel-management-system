a8a1210b36679ea36f1425c31d31aad4bf7f5177
SajidAhmed0 <mjsahmed205@gmail.com>
2024-03-02 21:17:14 +0530

    bookedprice entity

---
+++ src/main/java/com/hotelmangementsystem/application/entity/BookedPrice.java
@@ -0,0 +1,70 @@
+package com.hotelmangementsystem.application.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class BookedPrice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String type;
+
+    private String name;
+
+    private Double price;
+
+    //TODO: create booking
+
+    public BookedPrice() {
+    }
+
+    public BookedPrice(String type, String name, Double price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    public BookedPrice(Long id, String type, String name, Double price) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
