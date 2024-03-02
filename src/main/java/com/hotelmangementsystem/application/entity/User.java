b99dc3dba67d5fdb0219eacedabf7e37734d68f4
SajidAhmed0 <mjsahmed205@gmail.com>
2024-03-03 00:06:53 +0530

    change user table name to client

--- src/main/java/com/hotelmangementsystem/application/entity/User.java
+++ src/main/java/com/hotelmangementsystem/application/entity/User.java
@@ -1,11 +1,9 @@
 package com.hotelmangementsystem.application.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "client")
 public class User {
 
     @Id
