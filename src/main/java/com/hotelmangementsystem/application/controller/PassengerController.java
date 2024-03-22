4dff658c19c577dd95d8dc2f11376bef3a00ca13
SajidAhmed0 <mjsahmed205@gmail.com>
2024-03-23 00:15:52 +0530

    cross origin added

--- src/main/java/com/hotelmangementsystem/application/controller/PassengerController.java
+++ src/main/java/com/hotelmangementsystem/application/controller/PassengerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/passengers")
 public class PassengerController {
