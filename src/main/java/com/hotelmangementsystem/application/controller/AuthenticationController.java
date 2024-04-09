c6d244b44746b2c7d10768802d30a1f80d08f4af
SajidAhmed0 <mjsahmed205@gmail.com>
2024-04-09 21:18:05 +0530

    added cors to auth controller

--- src/main/java/com/hotelmangementsystem/application/controller/AuthenticationController.java
+++ src/main/java/com/hotelmangementsystem/application/controller/AuthenticationController.java
@@ -6,11 +6,9 @@ import com.hotelmangementsystem.application.dto.SignUpRequest;
 import com.hotelmangementsystem.application.entity.User;
 import com.hotelmangementsystem.application.service.AuthenticationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("auth")
 public class AuthenticationController {
